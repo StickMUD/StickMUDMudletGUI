e159bb44e22324289f28ff52bf26444b0bc0b826
Michael Conley <sousesider@gmail.com>
2024-06-30 11:34:17 -0400

    Initial commit

--- src/scripts/Globals.lua
+++ src/scripts/Globals.lua
@@ -1,3 +1,13 @@
 function firstToUpper(str)
     return (str:gsub("^%l", string.upper))
 end
+
+function enable_tooltip(whichLabel, message)
+	whichLabel:echo(message)
+end
+
+function disable_tooltip(whichLabel, message)
+	whichLabel:echo(message)
+end
+
+GUI = GUI or {}
