a67a1ef73b4e7fff169db734e2854273f8196eba
Michael Conley <sousesider@gmail.com>
2024-08-11 18:55:44 -0400

    Enhance: Font and FontSize handling

--- src/scripts/GMCP/RoomAndInventoryConsoles/Inventory/UpdateInventoryConsole.lua
+++ src/scripts/GMCP/RoomAndInventoryConsoles/Inventory/UpdateInventoryConsole.lua
@@ -2,46 +2,46 @@ wieldedWeapons = {}
 wornArmour = {}
 otherInventory = {}
 
-local function setupConsole(consoleName)
-    clearUserWindow(consoleName)
+local function setupConsole(console)
+    clearUserWindow(console)
 
     if gmcp.Game and gmcp.Game.Variables then
         local font = gmcp.Game.Variables.font
         if font and getAvailableFonts()[font] then
-            setFont(consoleName, font)
+            setFont(console, font)
         end
 
         local fontSize = gmcp.Game.Variables.fontSize
         if fontSize then
-            setMiniConsoleFontSize(consoleName, fontSize)
+            setMiniConsoleFontSize(console, fontSize)
         elseif getOS() == "mac" then
-            setMiniConsoleFontSize(consoleName, 10)
+            setMiniConsoleFontSize(console, 10)
         else
-            setMiniConsoleFontSize(consoleName, 8)
+            setMiniConsoleFontSize(console, 8)
         end
     end
 
-    _G[consoleName]:resetAutoWrap() -- Use _G to access the global variable by name
+    console:resetAutoWrap()
 end
 
-local function updateConsole(consoleName, header, inventory)
-    cecho(consoleName, "\n<light_blue:black>" .. header .. ":\n\n")
+local function updateConsole(console, header, inventory)
+    cecho(console, "\n<light_blue:black>" .. header .. ":\n\n")
     for key, value in pairs(inventory) do
-        echo(consoleName, " ")
-        echoLink(consoleName, "-", [[send("drop ]] .. key .. [[", false)]], "Drop", false)
-        echo(consoleName, " ( ")
-        echoLink(consoleName, "L", [[send("look at ]] .. key .. [[", false)]], "Look at", false)
-        cecho(consoleName, " ) " .. value .. "\n")
+        echo(console, " ")
+        echoLink(console, "-", [[send("drop ]] .. key .. [[", false)]], "Drop", false)
+        echo(console, " ( ")
+        echoLink(console, "L", [[send("look at ]] .. key .. [[", false)]], "Look at", false)
+        cecho(console, " ) " .. value .. "\n")
     end
 end
 
 function UpdateInventoryConsole()
-    setupConsole("GUI.WieldedWeaponsConsole")
-    updateConsole("GUI.WieldedWeaponsConsole", "Wielded Weapons", wieldedWeapons)
+    setupConsole(GUI.WieldedWeaponsConsole)
+    updateConsole(GUI.WieldedWeaponsConsole, "Wielded Weapons", wieldedWeapons)
 
-    setupConsole("GUI.WornArmourConsole")
-    updateConsole("GUI.WornArmourConsole", "Worn Armours", wornArmour)
+    setupConsole(GUI.WornArmourConsole)
+    updateConsole(GUI.WornArmourConsole, "Worn Armours", wornArmour)
 
-    setupConsole("GUI.InventoryConsole")
-    updateConsole("GUI.InventoryConsole", "Other Items", otherInventory)
+    setupConsole(GUI.InventoryConsole)
+    updateConsole(GUI.InventoryConsole, "Other Items", otherInventory)
 end
