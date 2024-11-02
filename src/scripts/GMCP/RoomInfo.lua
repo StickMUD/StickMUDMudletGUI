b03b958a99b8f7fc27969ed2a54ebe76123e823d
Michael Conley <sousesider@gmail.com>
2024-11-02 18:21:13 -0400

    Enhance: Add right images

--- src/scripts/GMCP/RoomInfo.lua
+++ src/scripts/GMCP/RoomInfo.lua
@@ -3,14 +3,12 @@ function RoomInfo()
     local area = gmcp.Room.Info.area or "Area"
     local exits = gmcp.Room.Info.exits or {}
 
-    GUI.BoxRoom:echo("<center><font size=\"4\"><img src=\"" ..
-                         getMudletHomeDir() ..
-                         "/StickMUD/047-user.png\"></font> <b><font size=\"3\">" ..
-                         name .. "</font></b></center>")
-    GUI.BoxArea:echo("<center><font size=\"4\"><img src=\"" ..
-                         getMudletHomeDir() ..
-                         "/StickMUD/048-tour.png\"></font> <b><font size=\"3\">" ..
-                         area .. "</font></b></center>")
+    GUI.BoxRoom:echo(
+        "<center><font size=\"4\">📍</font> <b><font size=\"3\">" .. name ..
+            "</font></b></center>")
+    GUI.BoxArea:echo(
+        "<center><font size=\"4\">🏰</font> <b><font size=\"3\">" .. area ..
+            "</font></b></center>")
 
     local directions = nil
 
@@ -50,8 +48,7 @@ function RoomInfo()
 
     if directions == nil then directions = "No obvious exits" end
 
-    GUI.BoxExits:echo("<center><font size=\"4\"><img src=\"" ..
-                          getMudletHomeDir() ..
-                          "/StickMUD/046-exit.png\"></font> <b><font size=\"3\">" ..
-                          directions .. "</font></b></center>")
+    GUI.BoxExits:echo(
+        "<center><font size=\"4\">🚪</font> <b><font size=\"3\">" ..
+            directions .. "</font></b></center>")
 end
--- src/scripts/GUI/Create_Footer.lua
+++ src/scripts/GUI/Create_Footer.lua
@@ -159,16 +159,15 @@ GUI.EnemyHealth:setValue(math.random(100), 100)
 
 GUI.BoxRoom = Geyser.Label:new({name = "GUI.BoxRoom"}, GUI.FooterBottom)
 GUI.BoxRoom:setStyleSheet(GUI.BoxFooterCSS:getCSS())
-GUI.BoxRoom:echo("<center><font size=\"4\"><img src=\"" .. getMudletHomeDir() ..
-                     "/StickMUD/047-user.png\"></font> <b><font size=\"3\">Room</font></b></center>")
+GUI.BoxRoom:echo(
+    "<center><font size=\"4\">📍</font> <b><font size=\"3\">Room</font></b></center>")
 
 GUI.BoxArea = Geyser.Label:new({name = "GUI.BoxArea"}, GUI.FooterBottom)
 GUI.BoxArea:setStyleSheet(GUI.BoxFooterCSS:getCSS())
-GUI.BoxArea:echo("<center><font size=\"4\"><img src=\"" .. getMudletHomeDir() ..
-                     "/StickMUD/048-tour.png\"></font> <b><font size=\"3\">Area</font></b></center>")
+GUI.BoxArea:echo(
+    "<center><font size=\"4\">🏰</font> <b><font size=\"3\">Area</font></b></center>")
 
 GUI.BoxExits = Geyser.Label:new({name = "GUI.BoxExits"}, GUI.FooterBottom)
 GUI.BoxExits:setStyleSheet(GUI.BoxFooterCSS:getCSS())
 GUI.BoxExits:echo(
-    "<center><font size=\"4\"><img src=\"" .. getMudletHomeDir() ..
-        "/StickMUD/046-exit.png\"></font> <b><font size=\"3\">Exits</font></b></center>")
+    "<center><font size=\"4\">🚪</font> <b><font size=\"3\">Exits</font></b></center>")
