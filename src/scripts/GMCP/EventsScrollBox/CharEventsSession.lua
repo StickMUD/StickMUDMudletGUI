82c42c6f974b2a78351551ca6e7220bc4f19e7d9
Michael Conley <sousesider@gmail.com>
2025-11-14 19:42:23 -0500

    Add debug output to diagnose event session data display issue
    - Log session event_id when received
    - Log area progress data when received
    - Log comparison between session event_id and active event id
    - This will help identify why session data isn't matching/displaying

--- src/scripts/GMCP/EventsScrollBox/CharEventsSession.lua
+++ src/scripts/GMCP/EventsScrollBox/CharEventsSession.lua
@@ -10,11 +10,21 @@ function CharEventsSession(event, gmcp_data)
     
     if not data or type(data) ~= "table" then
         eventsSessionData = {}
+        cecho("\n<red>DEBUG CharEventsSession: No data or not a table</red>")
         return
     end
     
     -- Store the session data
     eventsSessionData = data
+    
+    -- Debug output
+    cecho(string.format("\n<green>DEBUG CharEventsSession: Received event_id '%s'</green>", tostring(data.event_id)))
+    if data.areas_completed and data.areas_total then
+        cecho(string.format("\n<green>DEBUG CharEventsSession: Areas %d/%d</green>", data.areas_completed, data.areas_total))
+    end
+    if data.progress then
+        cecho(string.format("\n<green>DEBUG CharEventsSession: Progress table has %d areas</green>", #data.progress))
+    end
     
     -- Only rebuild the display if EventsScrollBox is currently selected
     if selected_console == "EventsScrollBox" then
