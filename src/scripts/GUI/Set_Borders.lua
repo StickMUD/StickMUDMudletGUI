a82adb2a279a6070ae38d9807d6d6e3756879504
Michael Conley <sousesider@gmail.com>
2025-11-27 09:45:16 -0500

    Add dynamic window resize handling
    - Create updateBorders() function to recalculate border sizes
    - Register sysWindowResizeEvent handler to update borders on resize
    - GUI now properly responds to Mudlet window size changes

--- src/scripts/GUI/Set_Borders.lua
+++ src/scripts/GUI/Set_Borders.lua
@@ -1,5 +1,14 @@
-local w,h = getMainWindowSize()
-setBorderLeft(0)
-setBorderTop(h/20)
-setBorderBottom(h/10)
-setBorderRight(w*0.42)
+-- Function to update borders based on current window size
+function updateBorders()
+    local w, h = getMainWindowSize()
+    setBorderLeft(0)
+    setBorderTop(h/20)
+    setBorderBottom(h/10)
+    setBorderRight(w*0.42)
+end
+
+-- Set initial borders
+updateBorders()
+
+-- Register handler for window resize events
+registerAnonymousEventHandler("sysWindowResizeEvent", "updateBorders")
