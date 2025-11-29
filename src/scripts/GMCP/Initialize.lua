5fe97f7d6d544018d5e5af8b49c22d135b1a292f
Michael Conley <sousesider@gmail.com>
2025-11-29 18:06:29 -0500

    Remove CoreHello.lua and its scripts.json entry

--- src/scripts/GMCP/Initialize.lua
+++ src/scripts/GMCP/Initialize.lua
@@ -25,6 +25,41 @@ tempTimer(
 )
 
 -- Request GMCP data from server (will update GUI when responses arrive)
+
+-- Declare supported GMCP packages to the server
+local packages = {
+    -- Core
+    "Core 1",
+
+    -- Character
+    "Char 1",
+    "Char.Vitals 1",          -- CharVitals.lua
+    "Char.Status 1",          -- CharStatus.lua
+    "Char.Items 1",           -- Inventory and Room item handlers
+    "Char.Abilities 1",       -- AbilitiesButtons handlers
+    "Char.Help 1",            -- HelpContainer/CharHelpList.lua
+    "Char.Guild.Help 1",      -- AbilitiesConsole/CharGuildHelpList.lua
+    "Char.Training 1",        -- TrainingScrollBox handlers
+    "Char.Events 1",          -- EventsScrollBox/CharEventsList.lua
+
+    -- Room
+    "Room 1",                 -- RoomInfo.lua, Room.Players handlers
+
+    -- Communication
+    "Comm 1",
+    "Comm.Channel 1",         -- CommChannelText.lua
+
+    -- Group
+    "Group 1",                -- GroupScrollBox/Group.lua
+
+    -- Game
+    "Game 1",
+    "Game.Players 1",         -- PlayersScrollBox handlers
+    "Game.Events 1",          -- EventsScrollBox/GameEvents handlers
+    "Game.Info 1",            -- InfoScrollBox/GameInfo.lua
+}
+sendGMCP("Core.Supports.Add " .. yajl.to_string(packages))
+
 sendGMCP("Char.Vitals")
 sendGMCP("Char.Status")
 sendGMCP("Game.Players.List")
